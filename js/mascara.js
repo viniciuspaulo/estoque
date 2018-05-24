f60c0bf3fc26954c409903e92ab495108d1b8616
Vinicius brito de paulo <vbbritodepaulo25@gmail.com>
2018-05-24 00:41:08 -0300

    teste

--- js/mascara.js
+++ js/mascara.js
@@ -4,8 +4,9 @@ $(document).ready(function(){
     $(".mask-cpf").mask("999.999.999-99");
     $('.mask-cnpj').mask("99.999.999/9999-99");
 
-    $('.mask-money').mask('000.000.000.000.000,00', {reverse: true});
-    $(".mask-money").change(function(){
-        $("#value").html($(this).val().replace(/\D/g,''))
-    })
+    $('.mask-money').maskMoney({
+        prefix: "R$:",
+        decimal: ",",
+        thousands: "."
+    });
 });
