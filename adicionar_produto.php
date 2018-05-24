33c0059409510424a58690ad5874a62cca4a31d6
Vinicius brito de paulo <vbbritodepaulo25@gmail.com>
2018-05-24 00:49:48 -0300

    teste

--- adicionar_produto.php
+++ adicionar_produto.php
@@ -1,6 +1,8 @@
-<?php include("conecta.php"); include("banco-produto.php");
+<?php 
 
-$nome =  $_POST["nome"];
+include "conecta.php" ; 
+
+$nome = $_POST["nome"];
 $preco = $_POST["preco"];
 $descricao = $_POST["descricao"];
 $categoria_id = $_POST['categoria_id'];
@@ -10,10 +12,14 @@ if(array_key_exists('usado', $_POST)) {
 	$usado = "false";
 }
 
-if(insereProduto($conexao, $nome, $preco, $descricao, $categoria_id, $usado)) {
+$sql = mysqli_query($conexao, "INSERT INTO produtos(nome, preco, descricao, categoria_id, usado,) 
+VALUES('$nome','$preco', '$descricao', '$categoria_id', '$usado')");
+
+if($sql){
+	echo "<center><h1> Cadastrado com sucesso </h1></center>";
 	header("Location: produtos-lista.php");
-} else {
-	header("Location: produtos-lista.php");
-}
+}else{
+	header("Location: cliente_cadastra.php");
+};
 
 mysqli_close($conexao);
