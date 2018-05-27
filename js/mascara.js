43a472d77afb436e72bd784e688132861f97a9ec
Vinicius brito de paulo <vbbritodepaulo25@gmail.com>
2018-05-27 17:17:17 -0300

    teste

--- js/mascara.js
+++ js/mascara.js
@@ -1,6 +1,7 @@
 
 $(document).ready(function(){
     $(".mask-cel").mask("(999) 99999-9999");
+    $(".mask-cep").mask("99999-999");
     $(".mask-cpf").mask("999.999.999-99");
     $('.mask-cnpj').mask("99.999.999/9999-99");
 
