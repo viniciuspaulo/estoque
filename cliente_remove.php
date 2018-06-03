62653a8fabbe00eb2043167e6a6e2a481da893fd
Vinicius brito de paulo <vbbritodepaulo25@gmail.com>
2018-06-03 14:47:41 -0300

    projeto

--- cliente_remove.php
+++ cliente_remove.php
@@ -3,7 +3,10 @@ include 'conecta.php';
 
 
 $cliente_id=$_POST['cliente_id'];
+
+$sql = mysqli_query($conexao, "DELETE FROM venda WHERE id_client = '$cliente_id' ");
 $sql = mysqli_query($conexao, "DELETE FROM clientes WHERE cliente_id = '$cliente_id' ");
+
 
 if($sql){
 	header("Location: cliente_lista.php");
