b8e4c97226eeb4616ef90e5c647d4f5f10d13448
Vinicius brito de paulo <vbbritodepaulo25@gmail.com>
2018-06-11 22:27:56 -0300

    validacao

--- menu-lateral.php
+++ menu-lateral.php
@@ -1,10 +1,17 @@
 <ul id="sidebar-nav">
     <li><a href="home.php"><span class="fa fa-home" style="font-size:20px"></span>Inicio</a></li>
+    
     <?php if ($_SESSION['perfil'] == '2') { ?>
-    <li><a href="funcionario_lista.php"><span class="fa fa-user" style="font-size:20px"></span>Funcionario</a></li>
+        <li><a href="funcionario_lista.php"><span class="fa fa-user" style="font-size:20px"></span>Funcionario</a></li>
     <?php }?>
-    <li><a href="cliente_lista.php"><span class="fa fa-users" style="font-size:20px"></span>Clientes</a></li>
-    <li><a href="fornecedores_lista.php"><span class="fa fa-truck" style="font-size:20px"></span>Fornecedores</a></li>
-    <li><a href="produtos-lista.php"><span class="fa fa-shopping-basket" style="font-size:20px"></span>Produtos</a></li>
-    <li><a href="vendas_lista.php"><span class="fa fa-shopping-cart" style="font-size:20px"></span>Vendas</a></li>
+
+    <?php if ($_SESSION['perfil'] != '3') { ?>
+        <li><a href="cliente_lista.php"><span class="fa fa-users" style="font-size:20px"></span>Clientes</a></li>
+    <?php }?>
+
+        <li><a href="fornecedores_lista.php"><span class="fa fa-truck" style="font-size:20px"></span>Fornecedores</a></li>
+        <li><a href="produtos-lista.php"><span class="fa fa-shopping-basket" style="font-size:20px"></span>Produtos</a></li>
+    <?php if ($_SESSION['perfil'] != '3') { ?>
+        <li><a href="vendas_lista.php"><span class="fa fa-shopping-cart" style="font-size:20px"></span>Vendas</a></li>
+    <?php }?>
 </ul>
