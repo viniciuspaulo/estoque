fccb20c0c3756864ff0bcef913622d2c8afc246c
Vinicius brito de paulo <vbbritodepaulo25@gmail.com>
2018-06-11 22:16:51 -0300

    validacao

--- cadastra_produtos.php
+++ cadastra_produtos.php
@@ -2,6 +2,8 @@
 <head>
 	<title>Painel admin</title>
 	<link rel="stylesheet" type="text/css" href="css/home.css"/>
+	<link rel="stylesheet" type="text/css" href="css/font-awesome.min.css"/>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	<link rel="stylesheet" type="text/css" href="css/style.css"/>
 	<link rel="stylesheet" type="text/css" href="css/font-awesome.css"/>
 	<style>
--- funcionario_cadastra.php
+++ funcionario_cadastra.php
@@ -1,7 +1,11 @@
 <html>
 <head>
 	<title>Painel admin</title>
+
 	<link rel="stylesheet" type="text/css" href="css/home.css"/>
+	<link rel="stylesheet" type="text/css" href="css/font-awesome.min.css"/>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
 	<link rel="stylesheet" type="text/css" href="css/style.css"/>
 	<link rel="stylesheet" type="text/css" href="css/cssutil.css"/>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
